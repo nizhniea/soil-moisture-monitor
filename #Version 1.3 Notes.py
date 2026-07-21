c2dcfd53163557b958aa9a298094120b3d7d0f8c
123abcln <nizhniea@mail.uc.edu>
2026-07-21 13:55:57 -0400

    #Version 1.3 Notes

---
+++ #Version 1.3 Notes.py
@@ -0,0 +1,9 @@
+#Version 1.3 Notes
+
+#Imported Panda in order to organize the data from the CSV.
+#The order is going to look like: 
+# CSV -> Pandas -> Matplotlib -> Graph
+#Imported Matplotlib in order to create graphs of the data.
+#Moved the CSV report outside of the while loop, so that the end result prints after user says "no".
+#Adjusted graph settings, and added coloring to make it look nicer. 
+#Also changed the percent ranges to make them evenly spread out, for the moisture status levels.

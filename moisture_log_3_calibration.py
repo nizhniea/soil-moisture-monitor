707524f0cbc4da6772d6b9066a85c6edbcfe6e93
123abcln <nizhniea@mail.uc.edu>
2026-08-29 00:28:00 -0400

    moisture_log_3_calibration.py

--- moisture_log_3_calibration.py
+++ moisture_log_3_calibration.py
@@ -1,1 +1,13 @@
-moisture_log_3_calibration.csv
+#First day: initial reading 60.6%
+#Added in 30mL, waited 30 mins, recorded again for a value of: 62.2%
+
+#Second day reading: 64.7%
+#No additional water has been added after the inital 30mL on the first day*
+
+#Third day: SKIPPED
+
+#Fourth day: 64%
+
+#Fifth day: 63.9%
+
+#Sixth day: 63.6%
